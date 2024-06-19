0ed2c53c13c8848b4d0ce5328e4913b597ab9cbe
Linkarzu <linkarzu@gmail.com>
2024-06-18 18:18:31 -0600

    debian docker convenience script removed unecessary comments, fixed formatting

--- debian/docker/10-convenience-script.sh
+++ debian/docker/10-convenience-script.sh
@@ -5,14 +5,10 @@ boldYellow="\033[1;33m"
 boldRed="\033[1;31m"
 noColor="\033[0m"
 
-# >>>>>>>>>>>>>Beginning<<<<<<<<<<<<
-printf "\n\n\n"
-printf "\n\n${boldGreen}>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n${noColor}"
-printf "${boldGreen}>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n\n${noColor}"
-printf "This script will\n"
+printf "\nThis script will\n"
 printf "1. Only install docker if not installed yet\n"
 printf "2. Check you have sudo permissions\n"
-printf "3. Ask you for the version you want to install\n"
+printf "3. Show you the versions and ask you which one you want to install\n"
 printf "4. Add your user to the docker group so you can run docker commands without sudo\n\n"
 
 # Only execute the script if docker is NOT installed
@@ -76,10 +72,10 @@ printf "${boldGreen}- Or if running a swarm cluster with 'docker node ls'\n${noC
 printf "${boldGreen}- Find releases in 'https://docs.docker.com/engine/release-notes'\n${noColor}"
 printf "${boldGreen}- Which comes from 'https://github.com/moby/moby/tags'\n${noColor}"
 
-printf "${boldYellow}Displaying list of latest 15 versions\n${noColor}"
-apt-cache madison docker-ce | awk '{ print $3 }' | head -n 15
-printf "${boldYellow}- Leave blank if you want to install the latest version\n\n${noColor}"
+printf "\n${boldYellow}Displaying list of latest 15 versions\n${noColor}"
+curl -s https://api.github.com/repos/moby/moby/tags | grep '"name"' | cut -d'"' -f4 | grep -E '^v' | grep -vE 'rc|beta' | tail -n 15
 
+printf "\n${boldYellow}Leave blank if you want to install the latest version\n\n${noColor}"
 read -p "Enter the Docker version to install (default: latest): " userInput
 
 printf "\n${boldYellow}Executing the downloaded script${noColor}\n"
