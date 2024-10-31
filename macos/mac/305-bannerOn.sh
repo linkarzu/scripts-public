d27d30dc05863d4311dae2e419596145e198eb4c
Linkarzu <linkarzu@gmail.com>
2024-10-30 19:58:55 -0600

    macos mac export path variable o bannerOn and bannerOff scripts

--- macos/mac/305-bannerOn.sh
+++ macos/mac/305-bannerOn.sh
@@ -3,12 +3,21 @@
 # Filename: ~/github/scripts-public/macos/mac/305-bannerOn.sh
 # ~/github/scripts-public/macos/mac/305-bannerOn.sh
 
+# This script is executed from karabiner, but in karabiner docs:
+# https://karabiner-elements.pqrs.org/docs/json/complex-modifications-manipulator-definition/to/shell-command/
+# The very limited environment variables are passed to the command, $HOME, $UID, $USER, etc.
+# Export environment variables in shell_command if your commands depend them.
+#
+# If you don't do this, the script won't find yabai or jq or any other apps in
+# the /opt/homebrew/bin dir
+export PATH="/opt/homebrew/bin:$PATH"
+
 # Function to show the banner
 show_banner() {
   touch ~/github/dotfiles-latest/youtube-banner.txt
   # tmux source-file ~/.tmux.conf
   # BTT does not recognize my path variable, so I specify the full path
-  /opt/homebrew/bin/tmux source-file ~/.tmux.conf
+  tmux source-file ~/.tmux.conf
   # sleep 0.10
 }
 
@@ -17,7 +26,7 @@ hide_banner() {
   rm ~/github/dotfiles-latest/youtube-banner.txt
   # tmux source-file ~/.tmux.conf
   # BTT does not recognize my path variable, so I specify the full path
-  /opt/homebrew/bin/tmux source-file ~/.tmux.conf
+  tmux source-file ~/.tmux.conf
   # sleep 0.10
 }
 
--- macos/mac/310-bannerOff.sh
+++ macos/mac/310-bannerOff.sh
@@ -3,11 +3,20 @@
 # Filename: ~/github/scripts-public/macos/mac/310-bannerOff.sh
 # ~/github/scripts-public/macos/mac/310-bannerOff.sh
 
+# This script is executed from karabiner, but in karabiner docs:
+# https://karabiner-elements.pqrs.org/docs/json/complex-modifications-manipulator-definition/to/shell-command/
+# The very limited environment variables are passed to the command, $HOME, $UID, $USER, etc.
+# Export environment variables in shell_command if your commands depend them.
+#
+# If you don't do this, the script won't find yabai or jq or any other apps in
+# the /opt/homebrew/bin dir
+export PATH="/opt/homebrew/bin:$PATH"
+
 rm ~/github/dotfiles-latest/youtube-banner.txt
 
 # tmux source-file ~/.tmux.conf
 # BTT does not recognize my path variable, so I specify the full path
-/opt/homebrew/bin/tmux source-file ~/.tmux.conf
+tmux source-file ~/.tmux.conf
 
 # sketchybar --reload
 # BTT does not recognize my path variable, so I specify the full path
